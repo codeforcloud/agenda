58417203c66a9696d3b5bfc12490151393d68695
Laurent Grangeau <laurent.grangeau@gmail.com>
2019-05-18 15:17:31 +0200

    Remove trailing whitespace
    Signed-off-by: Laurent Grangeau <laurent.grangeau@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ import HelloWorld from './components/HelloWorld.vue';
     HelloWorld,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue { }
 </script>
 
 <style>
